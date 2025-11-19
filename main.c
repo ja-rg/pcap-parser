b4b9180bcc73056d8771aac2272acae0b52e6220
J. Alex Rosales <j.alex.rosales@proton.me>
2025-11-19 13:55:37 -0600

    Se requiere archivo pcap por argumento

--- main.c
+++ main.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+#include "lib/pcap-master.h"
 
 
 int main(int argc, char *argv[])
 {
-
+    if (argc != 2)
+    {
+        fprintf(stderr, "Usage: %s <file.pcap>\n", argv[0]);
+        return 1;
+    }
 }
